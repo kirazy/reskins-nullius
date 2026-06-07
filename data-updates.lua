15ad57321d48130e7955e6e1e0fb73a09bf05579
Kira <kirazy@live.com>
2026-06-07 03:40:50 -0700

    feat: add belt types
    No support for custom colors yet.

---
+++ data-updates.lua
@@ -0,0 +1,3 @@
+require("prototypes.entities.splitters")
+require("prototypes.entities.transport-belts")
+require("prototypes.entities.underground-belts")
---
+++ prototypes/entities/underground-belts.lua
@@ -0,0 +1,20 @@
+local _framework = { tiers = require("__reskins-framework__.api.tiers") }
+
+local entities = {
+	["underground-belt"] = { type = "underground-belt", tier = 1, prog_tier = 1 },
+	["fast-underground-belt"] = { type = "underground-belt", tier = 2, prog_tier = 2 },
+	["express-underground-belt"] = { type = "underground-belt", tier = 3, prog_tier = 3 },
+	["bob-ultimate-underground-belt"] = { type = "underground-belt", tier = 4, prog_tier = 4 },
+}
+
+for name, options in pairs(entities) do
+	local entity = data.raw[options.type][name]
+	if not entity then
+		goto continue
+	end
+
+	local tier = _framework.tiers.get_tier(options)
+	_framework.tiers.add_tier_labels_to_prototype(tier, entity)
+
+	::continue::
+end
