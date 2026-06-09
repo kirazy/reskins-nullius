669189d0be3b22ac49f3e456ec93e9e9f2e4151c
Kira <kirazy@live.com>
2026-06-08 18:24:38 -0700

    fix: move to DFF to prevent collision with nullius overrides

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,6 +4,8 @@ require("prototypes.entities.pylons")
 require("prototypes.entities.sensor-nodes")
 require("prototypes.entities.substations")
 require("prototypes.entities.storage-tanks")
+require("prototypes.entities.inserters")
+require("prototypes.entities.inserters-bulk")
 
 -- After all entity updates.
 require("prototypes.items.broken")
